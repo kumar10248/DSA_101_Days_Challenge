9410bf808886548aaa16130ee224ab02b61b6807
kumar10248 <kumardevashish000@gmail.com>
2024-08-02 23:25:16 +0530

    solved two sum problem

---
+++ TwoSum.java
@@ -0,0 +1,14 @@
+class Solution {
+    public int[] twoSum(int[] nums, int target) {
+        for(int i=0;i<nums.length;i++){
+            for(int j=1+i;j<nums.length;j++){
+                if(nums[i]+nums[j]==target){
+                    int indices[]={i,j};
+                      return new int[]{i, j};
+                }
+              
+            }
+        }
+      throw new IllegalArgumentException("No two sum solution");    
+    }
+}
